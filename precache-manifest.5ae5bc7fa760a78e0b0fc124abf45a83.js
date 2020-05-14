3a3fd6e0f850c36b49277bc972ff43f0a7a53dc6
lazaro <lazaromello14@gmail.com>
2020-05-14 13:54:20 -0300

    Updates

---
+++ precache-manifest.5ae5bc7fa760a78e0b0fc124abf45a83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b10f39deffb23334e11e9c19f4f2bf59",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b0644f06b785aad2b1fa",
+    "url": "/monsters-rolodex/static/css/main.9fb13c8b.chunk.css"
+  },
+  {
+    "revision": "017031ba14a3fb92a040",
+    "url": "/monsters-rolodex/static/js/2.772e5a82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.772e5a82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0644f06b785aad2b1fa",
+    "url": "/monsters-rolodex/static/js/main.d8425fd6.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
